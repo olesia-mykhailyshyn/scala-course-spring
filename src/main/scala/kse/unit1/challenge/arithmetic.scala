af46fbb7df0bb640431670f6078ac89d305c0e4c
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2025-01-23 14:54:25 +0200

    addition, multiplication, and power functions were added

--- src/main/scala/kse/unit1/challenge/arithmetic.scala
+++ src/main/scala/kse/unit1/challenge/arithmetic.scala
@@ -23,20 +23,33 @@ object arithmetic:
       if isNonNegative(value) then value
       else -value
 
+  @tailrec
   def addition(left: Number, right: Number): Number =
     require(left >= 0, "Left must be non-negative")
     require(right >= 0, "Right must be non-negative")
 
-    ???
+    if isZero(left) then right
+    else if isZero(right) then left
+    else addition(decrement(left), increment(right))
 
   def multiplication(left: Number, right: Number): Number =
     require(left >= 0, "Left must be non-negative")
     require(right >= 0, "Right must be non-negative")
 
-    ???
+    if isZero(left) then 0
+    else if isZero(right) then 0
+    else if left == 1 then right
+    else if right == 1 then left
+    else addition(multiplication(left, decrement(right)),left)
 
   def power(base: Number, p: Number): Number =
     require(p >= 0, "Power must be non-negative")
     require(base != 0 || p != 0, "0^0 is undefined")
 
-    ???
+    @tailrec
+    def powerTailRec(base: Number, p: Number, intermResult: Number = 1): Number =
+      if p == 0 then intermResult
+      else if p == 1 then multiplication(intermResult, base)
+      else powerTailRec(base, decrement(p), multiplication(intermResult, base))
+
+    powerTailRec(base, p)
