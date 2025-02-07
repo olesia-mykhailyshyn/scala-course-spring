cdbc8a3b0eacbe61e2800a0dc540d919037b83ea
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2025-02-07 19:07:19 +0200

    unit-2 tasks

--- src/main/scala/kse/unit2/challenge/booleans.scala
+++ src/main/scala/kse/unit2/challenge/booleans.scala
@@ -7,44 +7,55 @@ object booleans:
   case object True
   case object False
 
-  type True  = True.type
-  type False = False.type
-  type Boolean
+  type True    = True.type
+  type False   = False.type
+  type Boolean = True | False
 
   val negation: Boolean => Boolean =
-    case True  => False.asInstanceOf[Boolean]
-    case False => True.asInstanceOf[Boolean]
+    case True  => False
+    case False => True
 
-//  val conjunction: (Boolean, => Boolean) => Boolean = ???
-//
-//  val disjunction: (Boolean, => Boolean) => Boolean = ???
-//
-//  val implication: (Boolean, => Boolean) => Boolean = ???
-//
-//  val equivalence: (Boolean, => Boolean) => Boolean = ???
-//
-//  extension (value: Boolean)
-//
-//    @targetName("negation")
-//    infix def unary_! : Boolean = ???
-//
-//    @targetName("conjunction")
-//    infix def ∧(that: => Boolean): Boolean = ???
-//
-//    @targetName("disjunction")
-//    infix def ∨(that: => Boolean): Boolean = ???
-//
-//    @targetName("implication")
-//    infix def →(that: => Boolean): Boolean = ???
-//
-//    @targetName("equivalence")
-//    infix def ↔(that: => Boolean): Boolean = ???
-//
-//  def fold(operation: (Boolean, Boolean) => Boolean, unit: Boolean)(list: List[Boolean]): Boolean = ???
-//
-//  val conjunctionOfElements: List[Boolean] => Boolean = ???
-//  val disjunctionOfElements: List[Boolean] => Boolean = ???
-//
-//  extension (booleans: List[Boolean])
-//    infix def conjunction: Boolean = conjunctionOfElements(booleans)
-//    infix def disjunction: Boolean = disjunctionOfElements(booleans)
+  val conjunction: (Boolean, => Boolean) => Boolean =
+    (value1, value2) => if value1 == False then False else value2
+
+  val disjunction: (Boolean, => Boolean) => Boolean =
+    (value1, value2) => if value1 == True then True else value2
+
+  val implication: (Boolean, => Boolean) => Boolean =
+    (value1, value2) => if value1 == False then True else value2
+
+  val equivalence: (Boolean, => Boolean) => Boolean =
+    (value1, value2) => if value1 == value2 then True else False
+
+  extension (value: Boolean)
+
+    @targetName("negation")
+    infix def unary_! : Boolean = negation(value)
+
+    @targetName("conjunction")
+    infix def ∧(that: => Boolean): Boolean = conjunction(value, that)
+
+    @targetName("disjunction")
+    infix def ∨(that: => Boolean): Boolean = disjunction(value, that)
+
+    @targetName("implication")
+    infix def →(that: => Boolean): Boolean = implication(value, that)
+
+    @targetName("equivalence")
+    infix def ↔(that: => Boolean): Boolean = equivalence(value, that)
+
+  def fold(operation: (Boolean, Boolean) => Boolean, unit: Boolean)(list: List[Boolean]): Boolean =
+    @tailrec
+    def foldReq(list: List[Boolean], acc: Boolean): Boolean =
+      list match
+        case Nil          => acc
+        case head :: tail => foldReq(tail, operation(head, acc))
+
+    foldReq(list, unit)
+
+  val conjunctionOfElements: List[Boolean] => Boolean = list => fold((a, b) => conjunction(a, b), True)(list)
+  val disjunctionOfElements: List[Boolean] => Boolean = list => fold((a, b) => disjunction(a, b), False)(list)
+
+  extension (booleans: List[Boolean])
+    infix def conjunction: Boolean = conjunctionOfElements(booleans)
+    infix def disjunction: Boolean = disjunctionOfElements(booleans)
--- src/test/scala/kse/unit2/challenge/BooleansSpecification.scala
+++ src/test/scala/kse/unit2/challenge/BooleansSpecification.scala
@@ -9,107 +9,135 @@ import org.scalacheck.Prop.{forAll, propBoolean}
 object BooleansSpecification extends Properties("Booleans"):
 
   include(NegationSpecification)
-//  include(ConjunctionSpecification)
-//  include(DisjunctionSpecification)
-//  include(ImplicationSpecification)
-//  include(EquivalenceSpecification)
-//  include(AxiomsSpecification)
-//  include(FoldSpecification)
+  include(ConjunctionSpecification)
+  include(DisjunctionSpecification)
+  include(ImplicationSpecification)
+  include(EquivalenceSpecification)
+  include(AxiomsSpecification)
+  include(FoldSpecification)
 
 end BooleansSpecification
 
 object NegationSpecification extends Properties("Negation"):
 
   property("!True is False") = propBoolean:
-    negation(False.asInstanceOf[Boolean]) == True
+    negation(True) == False
 
   property("!False is True") = propBoolean:
-    negation(False.asInstanceOf[Boolean]) == True
+    negation(False) == True
 
 end NegationSpecification
 
-//object ConjunctionSpecification extends Properties("Conjunction"):
-//
-//  property("True ∧ value is value") = forAll: (value: Boolean) =>
-//    ???
-//
-//  property("False ∧ value is False") = forAll: (value: Boolean) =>
-//    ???
-//
-//  property("False ∧ value should ignore the second argument") = forAll: (value: Boolean) =>
-//    ???
-//
-//end ConjunctionSpecification
-//
-//object DisjunctionSpecification extends Properties("Disjunction"):
-//
-//  property("True ∨ value is True") = forAll: (value: Boolean) =>
-//    ???
-//
-//  property("True ∨ value should ignore the second argument") = forAll: (value: Boolean) =>
-//    ???
-//
-//  property("False ∨ value is value") = forAll: (value: Boolean) =>
-//    ???
-//
-//end DisjunctionSpecification
-//
-//object ImplicationSpecification extends Properties("Implication"):
-//
-//  property("True → value is value") = forAll: (value: Boolean) =>
-//    ???
-//
-//  property("False → value is True") = forAll: (value: Boolean) =>
-//    ???
-//
-//  property("False → value should ignore the second argument") = forAll: (value: Boolean) =>
-//    ???
-//
-//end ImplicationSpecification
-//
-//// Optional challenge
-//object EquivalenceSpecification extends Properties("Equivalence"):
-//
-//end EquivalenceSpecification
-//
-//object AxiomsSpecification extends Properties("Axioms"):
-//
-//  property("a → (b → a)") = ???
-//
-//  property("(a → (b → c)) → ((a → b) → (a → c))") = ???
-//
-//  property("(a ∧ b) → a") = ???
-//
-//  property("(a ∧ b) → b") = ???
-//
-//  property("a → (b → (a ∧ b))") = ???
-//
-//  property("a → (a ∨ b)") = ???
-//
-//  property("b → (a ∨ b)") = ???
-//
-//  property("(a → c) → ((b → c) → ((a ∨ b) → c))") = ???
-//
-//  property("!a → (a → b)") = ???
-//
-//  property("(a → b) → ((a → !b) → !a)") = ???
-//
-//  property("a ∨ !a") = ???
-//
-//end AxiomsSpecification
-//
-//object FoldSpecification extends Properties("Fold"):
-//
-//  property("Conjunction of all elements from the empty list should be True") = propBoolean:
-//    Nil.conjunction == True
-//
-//  property("Conjunction of all elements from non-empty list should be correctly evaluated") = forAll: (booleans: List[Boolean]) =>
-//    booleans.conjunction == booleans.foldRight[Boolean](???)((value, acc) => value ∧ acc)
-//
-//  property("Disjunction of all elements from the empty list should be False") = propBoolean:
-//    Nil.disjunction == False
-//
-//  property("Disjunction of all elements from non-empty list should be correctly evaluated") = forAll: (booleans: List[Boolean]) =>
-//    booleans.disjunction == booleans.foldRight[Boolean](???)((value, acc) => value ∨ acc)
-//
-//end FoldSpecification
+object ConjunctionSpecification extends Properties("Conjunction"):
+
+  property("True ∧ value is value") = forAll: (value: Boolean) =>
+    conjunction(True, value) == value
+
+  property("False ∧ value is False") = forAll: (value: Boolean) =>
+    conjunction(False, value) == False
+
+  property("False ∧ value should ignore the second argument") = forAll: (value: Boolean) =>
+    conjunction(False, throw DummyError("Should not be thrown")) == False
+
+end ConjunctionSpecification
+
+object DisjunctionSpecification extends Properties("Disjunction"):
+
+  property("True ∨ value is True") = forAll: (value: Boolean) =>
+    disjunction(True, value) == True
+
+  property("True ∨ value should ignore the second argument") = forAll: (value: Boolean) =>
+    disjunction(True, throw DummyError("Should not be thrown")) == True
+
+  property("False ∨ value is value") = forAll: (value: Boolean) =>
+    disjunction(False, value) == value
+
+end DisjunctionSpecification
+
+object ImplicationSpecification extends Properties("Implication"):
+
+  property("True → value is value") = forAll: (value: Boolean) =>
+    implication(True, value) == value
+
+  property("False → value is True") = forAll: (value: Boolean) =>
+    implication(False, value) == True
+
+  property("False → value should ignore the second argument") = forAll: (value: Boolean) =>
+    implication(False, throw DummyError("Should not be thrown")) == True
+
+end ImplicationSpecification
+
+// Optional challenge
+object EquivalenceSpecification extends Properties("Equivalence"):
+  property("True ↔ True is True") = equivalence(True, True) == True
+
+  property("False ↔ False is True") = equivalence(False, False) == True
+
+  property("True ↔ False is False") = equivalence(True, False) == False
+
+  property("False ↔ True is False") = equivalence(False, True) == False
+end EquivalenceSpecification
+
+object AxiomsSpecification extends Properties("Axioms"):
+
+  property("a → (b → a)") = forAll { (a: Boolean, b: Boolean) =>
+    a → (b → a) == True
+  }
+
+  property("(a → (b → c)) → ((a → b) → (a → c))") = forAll { (a: Boolean, b: Boolean, c: Boolean) =>
+    (a → (b → c)) → ((a → b) → (a → c)) == True
+  }
+
+  property("(a ∧ b) → a") = forAll { (a: Boolean, b: Boolean) =>
+    (a ∧ b) → a == True
+  }
+
+  property("(a ∧ b) → b") = forAll { (a: Boolean, b: Boolean) =>
+    (a ∧ b) → b == True
+  }
+
+  property("a → (b → (a ∧ b))") = forAll { (a: Boolean, b: Boolean) =>
+    a → (b → (a ∧ b)) == True
+  }
+
+  property("a → (a ∨ b)") = forAll { (a: Boolean, b: Boolean) =>
+    a → (a ∨ b) == True
+  }
+
+  property("b → (a ∨ b)") = forAll { (a: Boolean, b: Boolean) =>
+    b → (a ∨ b) == True
+  }
+
+  property("(a → c) → ((b → c) → ((a ∨ b) → c))") = forAll { (a: Boolean, b: Boolean, c: Boolean) =>
+    (a → c) → ((b → c) → ((a ∨ b) → c)) == True
+  }
+
+  property("!a → (a → b)") = forAll { (a: Boolean, b: Boolean) =>
+    !a → (a → b) == True
+  }
+
+  property("(a → b) → ((a → !b) → !a)") = forAll { (a: Boolean, b: Boolean) =>
+    (a → b) → ((a → !b) → !a) == True
+  }
+
+  property("a ∨ !a") = forAll { (a: Boolean) =>
+    a ∨ !a == True
+  }
+
+end AxiomsSpecification
+
+object FoldSpecification extends Properties("Fold"):
+
+  property("Conjunction of all elements from the empty list should be True") = propBoolean:
+    Nil.conjunction == True
+
+  property("Conjunction of all elements from non-empty list should be correctly evaluated") = forAll: (booleans: List[Boolean]) =>
+    booleans.conjunction == booleans.foldRight[Boolean](True)((value, acc) => value ∧ acc)
+
+  property("Disjunction of all elements from the empty list should be False") = propBoolean:
+    Nil.disjunction == False
+
+  property("Disjunction of all elements from non-empty list should be correctly evaluated") = forAll: (booleans: List[Boolean]) =>
+    booleans.disjunction == booleans.foldRight[Boolean](False)((value, acc) => value ∨ acc)
+
+end FoldSpecification
