b6b05dc6ff347f34b9a898a852472fc5fdb1c1e0
olesia-mykhailyshyn <150893245+olesia-mykhailyshyn@users.noreply.github.com>
2025-03-01 14:23:48 +0200

    Unit 3 (#3)
    * conjunction, disjunction, equivalence are not implemented
    
    * all implementations were added
    
    * requested changes
    
    * test is fixed
    
    * mistakes were fixed
    
    * minor issues in Implication and Equivalence were fixed
    
    * code was refactored

--- src/main/scala/kse/unit3/challenge/expressions.scala
+++ src/main/scala/kse/unit3/challenge/expressions.scala
@@ -9,8 +9,8 @@ object expressions:
     def substitute(variable: Variable, expression: Expression): Expression
 
   sealed trait Boolean extends Expression:
-    val evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
+    val evaluate: Expression                                                 = this
+    def substitute(variable: Variable, substitution: Expression): Expression = this
 
   type True = True.type
   case object True extends Boolean
@@ -19,33 +19,68 @@ object expressions:
   case object False extends Boolean
 
   case class Variable(name: String) extends Expression:
-    val evaluate: Expression                                               = ???
-    def substitute(variable: Variable, expression: Expression): Expression = ???
+    val evaluate: Expression = this
+
+    def substitute(variable: Variable, expression: Expression): Expression =
+      if this == variable then expression else this
 
   case class Negation(expression: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+
+    def evaluate: Expression = expression.evaluate match
+      case True  => False
+      case False => True
+      case expr  => Negation(expr)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Negation(expression.substitute(variable, substitution))
+
+    override def toString: String = s"!$expression"
 
   case class Conjunction(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+
+    def evaluate: Expression =
+      (left.evaluate, right.evaluate) match
+        case (False, _) | (_, False) => False
+        case (True, expr)            => expr
+        case (expr, True)            => expr
+        case (l, r)                  => Conjunction(l, r)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Conjunction(left.substitute(variable, substitution), right.substitute(variable, substitution))
+
+    override def toString: String = s"($left ∧ $right)"
 
   case class Disjunction(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+
+    def evaluate: Expression =
+      (left.evaluate, right.evaluate) match
+        case (True, _) | (_, True) => True
+        case (False, expr)         => expr
+        case (expr, False)         => expr
+        case (l, r)                => Disjunction(l, r)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Disjunction(left.substitute(variable, substitution), right.substitute(variable, substitution))
+
+    override def toString: String = s"($left ∨ $right)"
 
   case class Implication(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+
+    def evaluate: Expression = Disjunction(Negation(left), right).evaluate
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Implication(left.substitute(variable, substitution), right.substitute(variable, substitution))
+
+    override def toString: String = s"($left → $right)"
 
   case class Equivalence(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+
+    def evaluate: Expression = Conjunction(Implication(left, right), Implication(right, left)).evaluate
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Equivalence(left.substitute(variable, substitution), right.substitute(variable, substitution))
+
+    override def toString: String = s"($left ↔ $right)"
 
   given Conversion[String, Variable] with
     def apply(str: String): Variable = Variable(str)
@@ -53,16 +88,16 @@ object expressions:
   extension (expr: Expression)
 
     @targetName("negation")
-    infix def unary_! : Negation = ???
+    infix def unary_! : Negation = Negation(expr)
 
     @targetName("conjunction")
-    infix def ∧(that: Expression): Conjunction = ???
+    infix def ∧(that: Expression): Conjunction = Conjunction(expr, that)
 
     @targetName("disjunction")
-    infix def ∨(that: Expression): Disjunction = ???
+    infix def ∨(that: Expression): Disjunction = Disjunction(expr, that)
 
     @targetName("implication")
-    infix def →(that: Expression): Implication = ???
+    infix def →(that: Expression): Implication = Implication(expr, that)
 
     @targetName("equivalence")
-    infix def ↔(that: Expression): Equivalence = ???
+    infix def ↔(that: Expression): Equivalence = Equivalence(expr, that)
